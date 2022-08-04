89ff437f6716142ea99e46e347d8d70462d5c8e8
Endah Sari <riri.mayries@gmail.com>
2022-08-04 18:54:44 +0700

    config database

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gofiber-crud
 Implementing CRUD in Golang REST API with Fiber
+
+# hot reload
+nodemon --exec go run main.go --signal SIGTERM
---
+++ database/connect.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+    "fmt"
+    "log"
+    "strconv"
+
+    "gofiber/crud/config"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+)
+
+// Declare the variable for the database
+var DB *gorm.DB
+
+// ConnectDB connect to db
+func ConnectDB() {
+    var err error
+    p := config.Config("DB_PORT")
+    port, err := strconv.ParseUint(p, 10, 32)
+
+    if err != nil {
+        log.Println("Unexpected error")
+    }
+
+		log.Println(config.Config("DB_HOST"))
+
+    // Connection URL to connect to Postgres Database
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_HOST"), port, config.Config("DB_NAME"))
+    // Connect to the DB and initialize the DB variable
+    DB, err = gorm.Open(mysql.Open(dsn))
+
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    fmt.Println("Connection Opened to Database")
+}
